0b4ea99f6b93d23ea11507efb131c46738ab21b2
kirillbodnar <91017699+kirillbodnar@users.noreply.github.com>
2022-04-22 17:08:18 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ function clearGallery() {
 
 function endOfImagesNotification(image) {
   if (image.data.hits.length === 0) {
-    window.removeEventListener('scroll', loadMoreImagesOnScroll);
+    window.removeEventListener('scroll', debounce(loadMoreImagesOnScroll, 400));
     return Notify.warning("We're sorry, but you've reached the end of search results.");
   }
 }
